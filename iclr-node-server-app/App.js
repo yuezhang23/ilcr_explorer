f49d8b012f8d0ee4d3633ddebb3a250dd6736807
yuezhang23 <jackiezy1002@gmail.com>
2025-08-17 16:07:21 -0400

    base API on home.ts

--- iclr-node-server-app/App.js
+++ iclr-node-server-app/App.js
@@ -35,7 +35,8 @@ mongoose.connection.on('disconnected', () => {
 });
 
 app.use(cors({
-  origin: [process.env.FRONTEND_URL, 'http://localhost:3000'],
+  // origin: [process.env.FRONTEND_URL, 'http://localhost:3000'],
+  origin: ['https://iclr-explorer.netlify.app/#/Home', 'http://localhost:3000'],
   credentials: true
 }));
 
